9688493c32814f973a08f49ec33ad9f2ba02d107
Alex Wardill <alexander@wardill.net>
2021-08-11 14:09:45 +0100

    drafted functionality

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Project #6 of Wes Bos' JS30
 
+- display data that includes text content
+- spread the city name into the results, li
+
+
+- find all the cities that include the search content
+- create a <li> for each of them
+- add it to the <ul>
--- app.js
+++ app.js
@@ -2,9 +2,28 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
 const form = document.getElementById('search-form');
 const suggestions = document.querySelector('.suggestions');
+const searchBox = document.getElementById('search-box');
 
 const cities = [];
 
 const prom = fetch(endpoint)
                 .then(stuff => stuff.json())
-                .then(data => cities = data);
+                .then(data => cities.push(data));
+
+
+function updateResults() {
+    let cityNames = cities
+                        .map(place => place.city)
+                        .filter(city => city.includes(searchBox.textContent));
+    let relevantCities = [...cityNames];
+    return relevantCities;
+};
+
+searchBox.addEventListener('change', () => {
+    updateResults();
+    relevantCities.forEach(element => {
+        let newDiv = document.createElement('div').textContent(element);
+        suggestions.appendChild(newDiv);
+    });
+});
+
